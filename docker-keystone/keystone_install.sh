65d2e6bf905aa27fe35d1e7fb4ad1048dc7dd3a2
zhangsirsdo <zhangsz8@163.com>
2017-01-10 15:28:16 +0800

    delete configuration in keystone.conf
    delete configuration in keystone.conf

--- docker-keystone/keystone_install.sh
+++ docker-keystone/keystone_install.sh
@@ -3,10 +3,6 @@ source /home/config.sh
 ADMIN_TOKEN=016f77abde58da9c724b
 source $PWD/env.sh
 
-openstack-config --set /etc/keystone/keystone.conf DEFAULT admin_token $ADMIN_TOKEN
-openstack-config --set /etc/keystone/keystone.conf token provider fernet
-openstack-config --set /etc/keystone/keystone.conf database connection "mysql+pymysql://keystone:$KEYSTONE_DBPASS@$MYSQL_IP/keystone"
-
 #chmod 777 /var/log/keystone/keystone.log
 su -s /bin/sh -c "keystone-manage db_sync" keystone
 
