944cfe4ab80c65b70bc5448a36a46b2f0badbc94
Luo Hao <luohao.brian@gmail.com>
2017-01-13 02:01:48 +0800

    Optimize mariadb init script
    a) Remove heat, malina init scripts
    b) Set default vals against global variables
    c) Not require 20s sleep by using "IF NOT EXISTS DBname"
    d) Rename global var names into a more common-sense style
    e) Support DB_PORT

--- docker-mariadb-init/mariadb_init.sh
+++ docker-mariadb-init/mariadb_init.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 
-if [ "$MARIADB_INIT" = "True" ];then
-  sleep 20
-  #keystone
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "CREATE DATABASE keystone;"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '$KEYSTONE_DBPASS';"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '$KEYSTONE_DBPASS';"
-  #glance
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "CREATE DATABASE glance;"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY '$GLANCE_DBPASS';"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY '$GLANCE_DBPASS';"
-  #nova
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "CREATE DATABASE nova;"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "CREATE DATABASE nova_api;"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY '$NOVA_DBPASS';"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY '$NOVA_DBPASS';"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'localhost' IDENTIFIED BY '$NOVA_DBPASS';"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'%' IDENTIFIED BY '$NOVA_DBPASS';"
-  #neutron
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "CREATE DATABASE neutron;"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'localhost' IDENTIFIED BY '$NEUTRON_DBPASS';"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'%' IDENTIFIED BY '$NEUTRON_DBPASS';"
-  #cinder
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "CREATE DATABASE cinder;"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'localhost' IDENTIFIED BY '$CINDER_DBPASS';"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'%' IDENTIFIED BY '$CINDER_DBPASS';"
-  #manila
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "CREATE DATABASE manila;"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON manila.* TO 'manila'@'localhost' IDENTIFIED BY '$MANILA_DBPASS';"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON manila.* TO 'manila'@'%' IDENTIFIED BY '$MANILA_DBPASS';"
-  #heat
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "CREATE DATABASE heat;"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'localhost' IDENTIFIED BY '$HEAT_DBPASS';"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'%' IDENTIFIED BY '$HEAT_DBPASS';"
-  #trove
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "CREATE DATABASE trove;"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON trove.* TO 'trove'@'localhost' IDENTIFIED BY '$TROVE_DBPASS';"
-  mysql -h $MYSQL_IP -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON trove.* TO 'trove'@'%' IDENTIFIED BY '$TROVE_DBPASS';"
-fi
+#globals
+DB_HOST:=localhost
+DB_PORT:=3306
+DB_USER:=root
+DB_PASS:=root
+KEYSTONE_DB_PASS:=$DB_PASS
+GLANCE_DB_PASS:=$DB_PASS
+NOVA_DB_PASS:=$DB_PASS
+NEUTRON_DB_PASS:=$DB_PASS
+CINDER_DB_PASS:=$DB_PASS
+
+#keystone
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "CREATE DATABASE keystone IF NOT EXISTS keystone;"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '$KEYSTONE_DB_PASS';"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '$KEYSTONE_DB_PASS';"
+
+#glance
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "CREATE DATABASE glance IF NOT EXISTS glance;"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY '$GLANCE_DB_PASS';"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY '$GLANCE_DB_PASS';"
+
+#nova
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "CREATE DATABASE nova IF NOT EXISTS nova;"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "CREATE DATABASE nova_api;"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY '$NOVA_DB_PASS';"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY '$NOVA_DB_PASS';"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'localhost' IDENTIFIED BY '$NOVA_DB_PASS';"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'%' IDENTIFIED BY '$NOVA_DB_PASS';"
+
+#neutron
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "CREATE DATABASE neutron IF NOT EXISTS neutron;"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'localhost' IDENTIFIED BY '$NEUTRON_DB_PASS';"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'%' IDENTIFIED BY '$NEUTRON_DB_PASS';"
+
+#cinder
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "CREATE DATABASE cinder IF NOT EXISTS cinder;"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'localhost' IDENTIFIED BY '$CINDER_DB_PASS';"
+mysql -h $DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT -e "GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'%' IDENTIFIED BY '$CINDER_DB_PASS';"
+
