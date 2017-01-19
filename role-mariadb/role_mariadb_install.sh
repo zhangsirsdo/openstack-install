f2a819282d3dd4da82deca3a84db49a87e70b297
zhangsirsdo <zhangsz8@163.com>
2017-01-19 16:59:41 +0800

    add openstack_mariadb_compose_template.yml openstack_mariadb_setup_compose_template.yml

--- role-mariadb/role_mariadb_install.sh
+++ role-mariadb/role_mariadb_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # install mariadb and mariadb_setup container
 
-export ETCD_HOST=${ETCD_HOST:=127.0.0.1}
-export ETCD_PORT=${ETCD_PORT:=2379}
-export DB_HOST=${DB_HOST:=$ETCD_HOST}
+ADVERTISEMENT_URL=`cat ../global.conf |grep ADVERTISEMENT_URL|awk -F '=' '{print $2}'`
+ADVERTISEMENT_URL=${ADVERTISEMENT_URL:=127.0.0.1:2379}
+export ADVERTISEMENT_URL
+export DB_HOST=${DB_HOST:=`echo $ADVERTISEMENT_URL|awk -F ':' '{print $1}'`}
 export DB_PORT=${DB_PORT:=3306}
 export DB_USER=${DB_USER:=root}
 export DB_PASS=${DB_PASS:=root}
@@ -15,50 +16,37 @@ export CINDER_DB_PASS=${CINDER_DB_PASS:=$DB_PASS}
 export DB_CONTAINER_NAME=${DB_CONTAINER_NAME:=mariadb_1}
 
 # generate compose file of mariadb
-cat <<EOF >./openstack_mariadb_compose.yml
-version: '2'
-services:
-  mariadb:
-    image: 'mariadb:10.1.20'
-    network_mode: "host"
-    environment:
-        - MYSQL_ROOT_PASSWORD=$DB_PASS
-EOF
-
+cp openstack_mariadb_compose_template.yml openstack_mariadb_compose.yml
+sed -i "s#DB_PASS#$DB_PASS#g" openstack_mariadb_compose.yml
+sed -i "s#MARIADB_PORT#$DB_PORT#g" openstack_mariadb_compose.yml
 # start mariadb container
 docker-compose -f ./openstack_mariadb_compose.yml up -d 
+rm -f openstack_mariadb_compose.yml
 
 # save params of mariadb to etcd
 sh ./role_mariadb_register.sh
 
 # detect whether the params of mariadb exist or not
-mariadb_status=`curl -L -XGET http://$ETCD_HOST:$ETCD_PORT/v2/keys/endpoints/mariadb/host`
+mariadb_status=`curl -L -XGET http://$ADVERTISEMENT_URL/v2/keys/endpoints/mariadb/host`
 res=$(echo $mariadb_status | grep "errorCode")
 while [ $res != "" ]
 do
   sleep 1
-  mariadb_status=`curl -L -XGET http://$ETCD_HOST:$ETCD_PORT/v2/keys/endpoints/mariadb/host`
+  mariadb_status=`curl -L -XGET http://$ADVERTISEMENT_URL/v2/keys/endpoints/mariadb/host`
   res=$(echo $mariadb_status | grep "errorCode")
 done
 
 # generate compose file of mariadb_setup
-cat <<EOF >./openstack_mariadb_setup_compose.yml
-version: '2'
-services:
-  mariadb_setup:
-    image: 'mariadb_setup:1'
-    network_mode: "host"
-    environment:
-        - DB_HOST=$DB_HOST
-        - DB_PORT=$DB_PORT
-        - DB_USER=$DB_USER
-        - DB_PASS=$DB_PASS
-        - KEYSTONE_DB_PASS=$KEYSTONE_DB_PASS
-        - GLANCE_DB_PASS=$GLANCE_DB_PASS
-        - NOVA_DB_PASS=$NOVA_DB_PASS
-        - NEUTRON_DB_PASS=$NEUTRON_DB_PASS
-        - CINDER_DB_PASS=$CINDER_DB_PASS
-EOF
-
+cp openstack_mariadb_setup_compose_template.yml openstack_mariadb_setup_compose.yml
+sed -i "s#DB_HOST_VAR#$DB_HOST#g" openstack_mariadb_setup_compose.yml
+sed -i "s#DB_PORT_VAR#$DB_PORT#g" openstack_mariadb_setup_compose.yml
+sed -i "s#DB_USER_VAR#$DB_USER#g" openstack_mariadb_setup_compose.yml
+sed -i "s#DB_PASS_VAR#$DB_PASS_VAR#g" openstack_mariadb_setup_compose.yml
+sed -i "s#KEYSTONE_DB_PASS_VAR#$KEYSTONE_DB_PASS#g" openstack_mariadb_setup_compose.yml
+sed -i "s#GLANCE_DB_PASS_VAR#$GLANCE_DB_PASS#g" openstack_mariadb_setup_compose.yml
+sed -i "s#NOVA_DB_PASS_VAR#$NOVA_DB_PASS#g" openstack_mariadb_setup_compose.yml
+sed -i "s#NEUTRON_DB_PASS_VAR#$NEUTRON_DB_PASS#g" openstack_mariadb_setup_compose.yml
+sed -i "s#CINDER_DB_PASS_VAR#$CINDER_DB_PASS#g" openstack_mariadb_setup_compose.yml
 # start mariadb_setup container
 docker-compose -f ./openstack_mariadb_setup_compose.yml up -d
+rm -f openstack_mariadb_setup_compose.yml
--- role-mariadb/role_mariadb_register.sh
+++ role-mariadb/role_mariadb_register.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # register service to etcd
 
-ETCD_HOST=${ETCD_HOST:=127.0.0.1}
-ETCD_PORT=${ETCD_PORT:=2379}
-DB_HOST=${DB_HOST:=$ETCD_HOST}
+DVERTISEMENT_URL=${ADVERTISEMENT_URL:=127.0.0.1:2379}
+DB_HOST=${DB_HOST:=`echo $ADVERTISEMENT_URL|awk -F ':' '{print $1}'`}
 DB_PORT=${DB_PORT:=3306}
 DB_USER=${DB_USER:=root}
 DB_PASS=${DB_PASS:=root}
@@ -14,12 +13,12 @@ NEUTRON_DB_PASS=${NEUTRON_DB_PASS:=$DB_PASS}
 CINDER_DB_PASS=${CINDER_DB_PASS:=$DB_PASS}
 
 # save params of mariadb to etcd
-curl -L -XPUT http://$ETCD_HOST:$ETCD_PORT/v2/keys/endpoints/mariadb/host -d value="$DB_HOST"
-curl -L -XPUT http://$ETCD_HOST:$ETCD_PORT/v2/keys/endpoints/mariadb/port -d value="$DB_PORT"
-curl -L -XPUT http://$ETCD_HOST:$ETCD_PORT/v2/keys/endpoints/mariadb/user -d value="$DB_USER"
-curl -L -XPUT http://$ETCD_HOST:$ETCD_PORT/v2/keys/endpoints/mariadb/pass -d value="$DB_PASS"
-curl -L -XPUT http://$ETCD_HOST:$ETCD_PORT/v2/keys/endpoints/mariadb/keystone_db_pass -d value="$KEYSTONE_DB_PASS"
-curl -L -XPUT http://$ETCD_HOST:$ETCD_PORT/v2/keys/endpoints/mariadb/glance_db_pass -d value="$GLANCE_DB_PASS"
-curl -L -XPUT http://$ETCD_HOST:$ETCD_PORT/v2/keys/endpoints/mariadb/nova_db_pass -d value="$NOVA_DB_PASS"
-curl -L -XPUT http://$ETCD_HOST:$ETCD_PORT/v2/keys/endpoints/mariadb/neutron_db_pass -d value="$NEUTRON_DB_PASS"
-curl -L -XPUT http://$ETCD_HOST:$ETCD_PORT/v2/keys/endpoints/mariadb/cinder_db_pass -d value="$CINDER_DB_PASS"
+curl -L -XPUT http://$ADVERTISEMENT_URL/v2/keys/endpoints/mariadb/host -d value="$DB_HOST"
+curl -L -XPUT http://$ADVERTISEMENT_URL/v2/keys/endpoints/mariadb/port -d value="$DB_PORT"
+curl -L -XPUT http://$ADVERTISEMENT_URL/v2/keys/endpoints/mariadb/user -d value="$DB_USER"
+curl -L -XPUT http://$ADVERTISEMENT_URL/v2/keys/endpoints/mariadb/pass -d value="$DB_PASS"
+curl -L -XPUT http://$ADVERTISEMENT_URL/v2/keys/endpoints/mariadb/keystone_db_pass -d value="$KEYSTONE_DB_PASS"
+curl -L -XPUT http://$ADVERTISEMENT_URL/v2/keys/endpoints/mariadb/glance_db_pass -d value="$GLANCE_DB_PASS"
+curl -L -XPUT http://$ADVERTISEMENT_URL/v2/keys/endpoints/mariadb/nova_db_pass -d value="$NOVA_DB_PASS"
+curl -L -XPUT http://$ADVERTISEMENT_URL/v2/keys/endpoints/mariadb/neutron_db_pass -d value="$NEUTRON_DB_PASS"
+curl -L -XPUT http://$ADVERTISEMENT_URL/v2/keys/endpoints/mariadb/cinder_db_pass -d value="$CINDER_DB_PASS"
